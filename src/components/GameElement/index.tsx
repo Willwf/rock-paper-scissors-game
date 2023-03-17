3f8289ecdc76a6bb24b2c7df92e929ce825be8ea
William Firmino <williamwf07@gmail.com>
2023-03-17 12:39:58 -0300

    feat: created Game element when a shape is selected

--- src/components/GameElement/index.tsx
+++ src/components/GameElement/index.tsx
@@ -1,8 +1,22 @@
+import { useState } from "react";
 import { HandShapeDivWrapper } from "../HandShapeDivWrapper";
 import * as Styles from "./styles";
 
 export function GameElement() {
-  return (
+  const [HandShapeSelected, setHandShapeSelected] = useState("rock");
+
+  return HandShapeSelected ? (
+    <Styles.GameElementResult>
+      <Styles.HandShapeSelectedDiv>
+        <HandShapeDivWrapper shape={HandShapeSelected} />
+        <Styles.HandShapeSubtitle>YOU PICKED</Styles.HandShapeSubtitle>
+      </Styles.HandShapeSelectedDiv>
+      <Styles.HandShapeSelectedDiv>
+        <Styles.HandShapeLoading />
+        <Styles.HandShapeSubtitle>THE HOUSE PICKED</Styles.HandShapeSubtitle>
+      </Styles.HandShapeSelectedDiv>
+    </Styles.GameElementResult>
+  ) : (
     <Styles.GameElement>
       <HandShapeDivWrapper shape="paper" />
       <HandShapeDivWrapper shape="scissors" />
--- src/components/GameElement/styles.ts
+++ src/components/GameElement/styles.ts
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import * as colors from "../../styles/variables"
 import triangleBg from "../../assets/images/bg-triangle.svg"
 
 export const GameElement = styled.div`
@@ -16,4 +15,36 @@ export const GameElement = styled.div`
     "handPaper handScissors"
     "handRock handRock";
   justify-items: center;
+`
+
+export const GameElementResult = styled.div`
+  min-width: 85%;
+  height: 42.5%;
+
+  display: flex;
+  justify-content: space-between;
+`
+
+export const HandShapeSelectedDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+export const HandShapeLoading = styled.div`
+  width: 13rem;
+  height: 13rem;
+
+  background-color: #0000001f;
+  border-radius: 50%;
+`
+
+
+export const HandShapeSubtitle = styled.p`
+  font-family: 'Barlow Semi Condensed', sans-serif;
+  font-size: 1.5rem;
+  color: white;
+  letter-spacing: .2rem;
+
+  margin-top: 2rem;
 `
